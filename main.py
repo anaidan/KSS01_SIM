c69b9ab8fd1b42f8416f8db8068df2a30f103fd7
Aidan Phillips <aidan.instance@gmail.com>
2021-02-24 20:44:04 -0800

    similar revs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import math
 from HUB import hub
 from FLO_HINGE import flo_hinge
 
-WIDTH = 600.0
+WIDTH = 800.0
 HEIGHT = 800.0
 UPD_TIME = 0.01
 
@@ -16,16 +16,16 @@ canvas.pack()
 hubs = []
 flo_hinges = []
 
-hubs.append(hub(60.0, HEIGHT - 60.0, 50.0, 15.0, 153.0, 0.0, canvas))
-hubs.append(hub(250, HEIGHT - 100, 45, hubs[0].rpm * hubs[0].radius / 45, 143.0, 0.0, canvas))
-hubs.append(hub(350, HEIGHT - 200, 30, hubs[1].rpm * hubs[1].radius / 30, 190.0, 0.0, canvas))
-hubs.append(hub(450, HEIGHT - 300, 60, hubs[2].rpm * hubs[2].radius / 60, 220.0, 0.0, canvas))
-hubs.append(hub(500, HEIGHT - 500, 40, hubs[3].rpm * hubs[3].radius / 40, 220.0, 0.0, canvas))
+hubs.append(hub(150.0, HEIGHT - 100.0, 40.0, 15.0, 150.0, 0.0, canvas))
+hubs.append(hub(250, HEIGHT - 200, 41, hubs[0].rpm * hubs[0].radius / 41, 180.0, 0.0, canvas))
+hubs.append(hub(350, HEIGHT - 300, 42, hubs[1].rpm * hubs[1].radius / 42, 180.0, 0.0, canvas))
+hubs.append(hub(450, HEIGHT - 400, 43, hubs[2].rpm * hubs[2].radius / 43, 180.0, 0.0, canvas))
+hubs.append(hub(550, HEIGHT - 500, 44, hubs[3].rpm * hubs[3].radius / 44, 180.0, 0.0, canvas))
 
-flo_hinges.append(flo_hinge(140, canvas))
-flo_hinges.append(flo_hinge(190, canvas))
-flo_hinges.append(flo_hinge(140, canvas))
-flo_hinges.append(flo_hinge(190, canvas))
+flo_hinges.append(flo_hinge(180, canvas))
+flo_hinges.append(flo_hinge(180, canvas))
+flo_hinges.append(flo_hinge(180, canvas))
+flo_hinges.append(flo_hinge(180, canvas))
 
 for i in range(0, len(hubs)):
     hubs[i].create_shapes()
