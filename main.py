61d75cc59a4ed6dc7c7266506d7408aec709e68a
Aidan Phillips <aidan.instance@gmail.com>
2021-02-24 20:12:55 -0800

    object instances now organized in lists, much cleaner

--- main.py
+++ main.py
@@ -13,67 +13,38 @@ window.tk.call('tk', 'scaling', .50)
 canvas = Canvas(window, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-hub_1 = hub(60.0, HEIGHT - 60.0, 50.0, 15.0, 153.0, 0.0, canvas)
-hub_2 = hub(250, HEIGHT - 100, 45, hub_1.rpm * hub_1.radius / 45, 143.0, 0.0, canvas)
-hub_3 = hub(350, HEIGHT - 200, 30, hub_2.rpm * hub_2.radius / 30, 190.0, 0.0, canvas)
-hub_4 = hub(450, HEIGHT - 300, 60, hub_3.rpm * hub_3.radius / 60, 220.0, 0.0, canvas)
-hub_5 = hub(500, HEIGHT - 500, 40, hub_4.rpm * hub_4.radius / 40, 220.0, 0.0, canvas)
+hubs = []
+flo_hinges = []
 
-flo_hinge_1 = flo_hinge(140, canvas)
-flo_hinge_2 = flo_hinge(190, canvas)
-flo_hinge_3 = flo_hinge(140, canvas)
-flo_hinge_4 = flo_hinge(5, canvas)
+hubs.append(hub(60.0, HEIGHT - 60.0, 50.0, 15.0, 153.0, 0.0, canvas))
+hubs.append(hub(250, HEIGHT - 100, 45, hubs[0].rpm * hubs[0].radius / 45, 143.0, 0.0, canvas))
+hubs.append(hub(350, HEIGHT - 200, 30, hubs[1].rpm * hubs[1].radius / 30, 190.0, 0.0, canvas))
+hubs.append(hub(450, HEIGHT - 300, 60, hubs[2].rpm * hubs[2].radius / 60, 220.0, 0.0, canvas))
+hubs.append(hub(500, HEIGHT - 500, 40, hubs[3].rpm * hubs[3].radius / 40, 220.0, 0.0, canvas))
 
-hub_1.create_shapes()
-hub_2.create_shapes()
-hub_3.create_shapes()
-hub_4.create_shapes()
-hub_5.create_shapes()
+flo_hinges.append(flo_hinge(140, canvas))
+flo_hinges.append(flo_hinge(190, canvas))
+flo_hinges.append(flo_hinge(140, canvas))
+flo_hinges.append(flo_hinge(190, canvas))
 
-flo_hinge_1.create_shapes()
-flo_hinge_2.create_shapes()
-flo_hinge_3.create_shapes()
-flo_hinge_4.create_shapes()
+for i in range(0, len(hubs)):
+    hubs[i].create_shapes()
 
-
+for i in range(0, len(flo_hinges)):
+    flo_hinges[i].create_shapes()
 
 while True:
-    hub_1.update(flo_hinge_1)
-    hub_2.update(flo_hinge_1)
-    hub_3.update(flo_hinge_2)
-    hub_4.update(flo_hinge_3)
-    hub_5.update(flo_hinge_4)
+    hubs[0].update(flo_hinges[0])
+    for i in range(1, len(hubs)):
+        hubs[i].update(flo_hinges[i-1])
 
-    flo_hinge_1.calc_rod_intersect(True, hub_1, hub_2)
-    flo_hinge_2.calc_rod_intersect(False, flo_hinge_1, hub_3)
-    flo_hinge_3.calc_rod_intersect(False, flo_hinge_2, hub_4)
-    flo_hinge_4.calc_rod_intersect(False, flo_hinge_3, hub_5)
+    flo_hinges[0].calc_rod_intersect(True, hubs[0], hubs[1])
+    for i in range(1, len(flo_hinges)):
+        flo_hinges[i].calc_rod_intersect(False, flo_hinges[i-1], hubs[i+1])
 
-    flo_hinge_1.update(flo_hinge_2)
-    flo_hinge_2.update(flo_hinge_3)
-    flo_hinge_3.update(flo_hinge_4)
-    flo_hinge_4.update(None)
-
-
-    # hub_1.draw_rod()
-
-    # xt, yt = hub_1.calc_rod_intersect(hub_2)
-    # xf, yf = hub_1.rod_joint_pos
-    # xf2, yf2 = hub_2.rod_joint_pos
-    # canvas.coords(int1, xt - 1, yt - 1, xt + 1, yt + 1)
-    # hub_1.draw_rod(xf, yf, xt, yt)
-    # hub_2.draw_rod(xf2, yf2, xt, yt)
-    #
-    # xp, yp = hub_3.rod_joint_pos
-    # xq, yq = calc_rod_int(xt, yt, xp, yp, 140, hub_3.rod_len)
-    # canvas.coords(p2, xq-1, yq-1, xq+1, yq+1)
-    #
-    # canvas.coords(l3, xp, yp, xq, yq)
-    # canvas.coords(l2, xt, yt, xq, yq)
-
-
-    # print(xt)
-    # print(yt)
+    for i in range(0, len(flo_hinges)-1):
+        flo_hinges[i].update(flo_hinges[i+1])
+    flo_hinges[3].update(None)
 
     window.update()
     time.sleep(UPD_TIME)
